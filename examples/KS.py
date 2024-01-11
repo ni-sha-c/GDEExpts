80b52bf3591fb0f05a23114451f3ccdfe9705d0d
Jeongjin Park <jpark3141@gatech.edu>
2024-01-11 14:06:32 -0500

    KS

--- examples/KS.py
+++ examples/KS.py
@@ -1,38 +1,101 @@
-class KuramotoSivashinsky():
-    def __init__(
-        self,
-        L,
-        N,
-        dt,
-    ):
-        self.L = L
-        self.N = N
-        self.dt = dt
-        self.dx = L / N
+import torch
 
-        wavenumbers = jnp.fft.rfftfreq(N, d=L / (N * 2 * jnp.pi))
-        self.derivative_operator = 1j * wavenumbers
+def KuramotoSivashinsky(u, c, dt, L):
+    # From ... paper
 
-        linear_operator = - self.derivative_operator**2 - self.derivative_operator**4
-        self.exp_term = jnp.exp(dt * linear_operator)
-        self.coef = jnp.where(
-            linear_operator == 0.0,
-            dt,
-            (self.exp_term - 1.0) / linear_operator,
-        )
+    # 1-D modified KS equation
+    # du/dt = -(u+c)du/dx - d^2u/dx^2 - d^4u/dx^4
+    # x is in [0, L]
+    # N is the number of points calculated along x
+    # Boundary condition with u(0,t) = u(L,t) = 0
 
-        self.alias_mask = (wavenumbers < 2/3 * jnp.max(wavenumbers))
-    
-    def __call__(
-        self,
-        u,
-    ):
-        u_nonlin = - 0.5 * u**2
-        u_hat = jnp.fft.rfft(u)
-        u_nonlin_hat = jnp.fft.rfft(u_nonlin)
-        u_nonlin_hat = self.alias_mask * u_nonlin_hat
-        u_nonlin_der_hat = self.derivative_operator * u_nonlin_hat
+    # Total number of nodes
+    # i = 0, 1, 2, ... n, n+1
+    n = 127 # number of interior nodes, i = 1, 2, ..., n
+    dx = L/(n+1)
+    dx_square = dx**2
+    dx_fourth = dx**4
 
-        u_next_hat = self.exp_term * u_hat + self.coef * u_nonlin_der_hat
-        u_next = jnp.fft.irfft(u_next_hat, n=self.N)
-        return u_next
+    # Matrix A
+    A = torch.zeros(128, 128)
+    A[range(1, A.shape[0]), range(0, A.shape[1] - 1)] = -1  
+    A[range(0, A.shape[0] - 1), range(1, A.shape[1])] = 1  
+    print(A)
+
+    # Matrix B
+    B = torch.zeros(128, 128)
+    B[range(1, B.shape[0]), range(0, B.shape[1] - 1)] = 1
+    B[range(0, B.shape[0] - 1), range(1, B.shape[1])] = 1
+    B[range(0, B.shape[0]), range(0, B.shape[1])] = -2
+    print(B)
+
+    # Matrix C
+    C = torch.zeros(128,128)
+    C[range(1, C.shape[0]), range(0, C.shape[1] - 1)] = -4
+    C[range(0, C.shape[0] - 1), range(1, C.shape[1])] = -4
+    C[range(2, C.shape[0]), range(0, C.shape[1] - 2)] = 1
+    C[range(0, C.shape[0] - 2), range(2, C.shape[1])] = 1
+    C[range(0, C.shape[0]), range(0, C.shape[1])] = 6
+    print(C[0:3, :6])
+
+
+    # first term: dudx
+    first_term = 1/(2*dx) * torch.matmul(A, u)
+    print("ft", first_term.shape)
+
+    # second term: ududx
+    u_square = u * u # Element-wise mult
+    print("square", u[:2], u_square[:2])
+    second_term = 1/(4*dx) * torch.matmul(A, u_square)
+
+    # third term: d^2u/dx^2
+    third_term = 1/dx_square * torch.matmul(B, u)
+
+    # fourth term: d^4/dx^4
+    fourth_term = 1/dx_fourth * torch.matmul(C, u)
+
+    # dudt
+    dudt = -second_term - c*first_term - third_term - fourth_term
+    print(dudt)
+
+    # Boundary Condition
+    dudt[0] = 0
+    dudt[-1] = 0
+
+    # compute dudt[1]
+    f_1 = u[2]/(2*dx)
+    f_2 = u_square[2]/(4*dx_square)
+    f_3 = (u[2]-u[1])/dx_square
+    f_4 = (7*u[1]-4*u[2]+u[3])/dx_fourth
+    dudt[1] = -f_2 - c*f_1 - f_3 - f_4    
+
+    # compute dudt[n]
+    n_1 = -u[n-1]/(2*dx)
+    n_2 = -u_square[n-1]/(4*dx_square)
+    n_3 = (u[n-1]-2*u[n])/dx_square
+    n_4 = (7*u[n]-4*u[n-1]+u[n-2])/dx_fourth
+    dudt[n] = -n_2 - c*n_1 - n_3 - n_4  
+
+    # Rest
+    second_4 = (-4*u[1] + 6*u[2]-4*u[3]+u[4])/dx_fourth
+    second_last_4 = (-4*u[n] + 6*u[n-1]-4*u[n-2]+u[n-3])/dx_fourth
+
+    dudt[2] = dudt[2] - second_4
+    dudt[n-1] = dudt[n-1] - second_last_4
+
+    return dudt
+
+
+if __name__ == '__main__':
+    L = 128
+
+    u = torch.randn(L).T # [L, 1]
+    print("u shape", u.shape)
+    print("u", u[:2])
+    u[0] = 0
+    u[-1] = 0
+
+    c = 0.4
+    dt = 0.1
+
+    KS = KuramotoSivashinsky(u, c, dt, L)
