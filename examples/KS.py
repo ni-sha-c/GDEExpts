take ensemble averages, fix imex

examples/KS.py
@@ -14,7 +14,8 @@ def rhs_KS_implicit(u, dx):
     A = A.to_dense()
 
     # ----- fourth derivative ----- #
-    B = tosp.spdiags(torch.vstack((ones(n), -4*ones(n), 6*ones(n), -4*ones(n), ones(n)))/(dx*dx*dx*dx), torch.tensor([-2, -1, 0, 1, 2]), (n-2, n-2))
+    dx4 = dx*dx*dx*dx
+    B = tosp.spdiags(torch.vstack((ones(n), -4*ones(n), 6*ones(n), -4*ones(n), ones(n)))/dx4, torch.tensor([-2, -1, 0, 1, 2]), (n-2, n-2))
     B = B.to_dense()
 
     # Create the pad 
@@ -22,23 +23,19 @@ def rhs_KS_implicit(u, dx):
     C[1:n-1, 1:n-1] = B
 
     # Boundary Condition (i = 2, 3, ... , n-1)
-    C[1, :] = 0
-    C[-2, :] = 0
-    C[1, 1] = 7/(dx*dx*dx*dx)
-    C[1, 2] = -4/(dx*dx*dx*dx)
-    C[1, 3] = 1/(dx*dx*dx*dx)
-    C[-2, -2] = 7/(dx*dx*dx*dx)
-    C[-2, -3] = -4/(dx*dx*dx*dx)
-    C[-2, -4] = 1/(dx*dx*dx*dx)
+    C[1, 1] = 7/dx4
+    C[1, 2] = -4/dx4
+    C[1, 3] = 1/dx4
+    C[-2, -2] = 7/dx4
+    C[-2, -3] = -4/dx4
+    C[-2, -4] = 1/dx4
 
-    print("C after addition", C)
-
+    #print("Second  last row of C", C[-2, -5:]*dx4)
     # A += C
-
-    implicit_dudt = -torch.matmul(A, u)
-
-    return implicit_dudt
-    # return A
+    #print(torch.dot(C[-2, :], u))
+    #implicit_dudt = -torch.matmul(A, u)
+    #implicit_dudt -= torch.matmul(C, u)
+    return -(A+C)
 
 
 def rhs_KS_explicit_nl(u, c, dx):
@@ -47,7 +44,6 @@ def rhs_KS_explicit_nl(u, c, dx):
 
     B = tosp.spdiags(torch.vstack((ones(n), -ones(n)))/(2*dx), torch.tensor([1,-1]), (n, n))
     B = B.to_dense()
-    print("B", B)
 
     exp_term = - torch.matmul(B, u*u)/2
     exp_term[0], exp_term[-1] = 0., 0. # du_0/dx = 0, du_n/dx = 0
@@ -57,12 +53,10 @@ def rhs_KS_explicit_nl(u, c, dx):
 def rhs_KS_explicit_linear(u, c, dx):
     # u contains boundary nodes
     n = u.shape[0]
-    print("n", n)
 
     B = tosp.spdiags(torch.vstack((ones(n), -ones(n)))/(2*dx), torch.tensor([1,-1]), (n, n))
     B = B.to_dense()
     # du_0/dx = 0, du_n/dx = 0
-    print("B", B)
 
     exp_term = - torch.matmul(B*c, u)
  
@@ -72,19 +66,30 @@ def explicit_rk(u, c, dx, dt):
     k1 = rhs_KS_explicit_nl(u, c, dx) + rhs_KS_explicit_linear(u, c, dx)
     k2 = rhs_KS_explicit_nl(u + dt/3*k1, c, dx) + rhs_KS_explicit_linear(u + dt/3*k1, c, dx)
     k3 = rhs_KS_explicit_nl(u + dt*k2, c, dx) + rhs_KS_explicit_linear(u + dt*k2, c, dx)
-    k4 = rhs_KS_explicit_nl(u + dt*(0.75*k2 + 0.25*k3), c, dx) + rhs_KS_explicit(u + dt*(0.75*k2 + 0.25*k3), c, dx)
+    k4 = rhs_KS_explicit_nl(u + dt*(0.75*k2 + 0.25*k3), c, dx) + rhs_KS_explicit_linear(u + dt*(0.75*k2 + 0.25*k3), c, dx)
     return dt*(3/4*k2 - 1/4*k3 + 1/2*k4)
 
 def implicit_rk(u, c, dx, dt):
     n = u.shape[0]
-    Au = rhs_KS_implicit(u, dx)
-    # Au = torch.matmul(A, u)
+    A = rhs_KS_implicit(u, dx)
+    Au = torch.matmul(A, u)
     k2 = torch.linalg.solve(eye(n) - dt/3*A, Au)
     k3 = torch.linalg.solve(eye(n) - dt/2*A, Au + dt/2*matmul(A, k2))
     k4 = torch.linalg.solve(eye(n) - dt/2*A, Au + dt/4*matmul(A, 3*k2-k3))
     return dt * (3/4*k2 - 1/4*k3 + 1/2*k4)
 
 
+def run_KS(u, c, dx, dt, T):
+    # u contains boundary nodes
+    n = u.shape[0]
+    t = 0
+    while t < T:
+        u = u + explicit_rk(u, c, dx, dt) + implicit_rk(u, c, dx, dt) 
+        u[0], u[-1] = 0., 0.
+        t += dt
+        print("At time", t, " Max u", torch.max(u))
+        print("Min u", torch.min(u))
+    return u
 
 """
 def KuramotoSivashinsky(u, c, dt, L):
@@ -173,11 +178,11 @@ def KuramotoSivashinsky(u, c, dt, L):
     return dudt
 """
 
-if __name__ == '__main__':
-    u = torch.arange(10).float()
-    dx = 1.
-    c = 1
-    A = rhs_KS_explicit(u, c, dx)
+#if __name__ == '__main__':
+#    u = torch.arange(10).float()
+#    dx = 1.
+#    c = 1
+#    A = rhs_KS_explicit(u, c, dx)
     # print(second_derivative(u, dx))
     #L = 128
 

test/test_KS.py
@@ -17,39 +17,58 @@ def test_KuramotoSivashinsky():
           i = -1, n+2 are ghost nodes
     '''
 
-    L = 128 # signal from [0, L]
+    L = pi # signal from [0, L]
     n = 127 # n = number of interior nodes: 127, 511
     dx = L/(n+1) # 0.25
     c = 0.4
     x = torch.arange(0, L+dx, dx) # [0, 0+dx, ... 128] shape: L + 1
-    print(x[0:3], x[-1], x.shape)
-
-    u = sin(2*pi*x/L)
+    u = -0.5 + rand(n+2)
+    #u = sin(2*pi*x/L)
+    u = x**4  
     u[0], u[-1] = 0, 0 # u_0, u_n = 0, 0
-    print("u", u)
 
-    up = cos(2*pi*x/L)*2*pi/L
-    upup = -sin(2*pi*x/L)*(2*pi/L)**2
-    upupup = -cos(2*pi*x/L)*(2*pi/L)**3
-    upupupup = sin(2*pi*x/L)*(2*pi/L)**4
+    print((7*u[n] - 4*u[n-1] + u[n-2])/(dx**4))
+    up = cos(x)
+    upup = -sin(x)
+    upupup = -cos(x)
+    upupupup = sin(x)
 
     # --- ana_rhs_KS: -(u + c)*up - upup - upupupup --- #
-    # ana_rhs_KS = -upup
-
+    #ana_rhs_KS = -upupupup
+    ana_rhs_KS = 24.0*ones_like(x)
     # --- num_rhs_KS: rhs_KS(u, c, dx) --- #
-    # num_rhs_KS = rhs_KS_implicit(u, dx)
-    num_rhs_KS = rhs_KS_explicit_nl(u, c,dx)
+    num_rhs_KS = rhs_KS_implicit(u, dx)
+    #num_rhs_KS = rhs_KS_explicit_nl(u, c,dx)
     # rhs_KS_explicit(u, c,dx)
     #  + rhs_KS_explicit_linear(u, c, dx)
  
     # Testing for inner nodes
-    print("answer", ana_rhs_KS[0:5], ana_rhs_KS[-5:])
-    print("predicted", num_rhs_KS[0:5], num_rhs_KS[-5:])
+    print("answer", ana_rhs_KS[:10])
+    print("predicted", num_rhs_KS[:10])
+
     print(norm(ana_rhs_KS[1:-1]))
     print(norm(num_rhs_KS[1:-1]))
-    assert np.allclose(ana_rhs_KS[1:-1], num_rhs_KS[1:-1], rtol=1e-5, atol=1e-5)
+    #assert np.allclose(ana_rhs_KS[1:-1], num_rhs_KS[1:-1], rtol=1e-5, atol=1e-5)
     
-    return
+    return u, num_rhs_KS, ana_rhs_KS
+
+def KS_FD_Simulate_ensemble(c, T, N):
+    L = 128 # signal from [0, L]
+    n = 127 # n = number of interior nodes: 127, 511
+    dx = L/(n+1) # 0.25
+
+    dt = 0.1 
+    x = torch.arange(0, L+dx, dx) # [0, 0+dx, ... 128] shape: L + 1
+    u0 = torch.zeros(N, n+2)
+    for i in range(N):
+        u0[i] = -0.5 + rand(n+2)
+        #u = sin(2*pi*x/L)
+        u0[i,0], u0[i,-1] = 0, 0
+        u[i,:] = u0[i]
+        u1 = run_KS(u[i,:], c, dx, dt, T)
+        u[i,:] = u1
+    return x, u0, u1
+
 
 
 
@@ -116,8 +135,16 @@ def KS_Simulate():
     return tt, uu, x
 
 if __name__ == '__main__':
-    test_KuramotoSivashinsky()
-
+    #test_KuramotoSivashinsky()
+    fig, ax = plt.subplots()
+    c = 0
+    x, u, u1 = KS_FD_Simulate(c,50, 100)
+    print("Average of u", torch.mean(u))
+    ax.plot(x, u, 'b', label='initial u')
+    ax.plot(x, u1, 'r', label='final u')
+    ax.legend(fontsize=18)
+    ax.xaxis.set_tick_params(labelsize=18)
+    ax.yaxis.set_tick_params(labelsize=18)
     '''L = 128
     n = 511 # number of interior nodes
     c = 0.